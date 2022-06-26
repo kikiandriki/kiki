01de92f7b34a20b275e9ab53841827a3af8e7641
Mateja Lasan <m.vuk.lasan@icloud.com>
2022-06-26 00:51:41 +0000

    Track emote usage

--- kiki/modules/ladder/cogs.py
+++ kiki/modules/ladder/cogs.py
@@ -1,9 +1,14 @@
 """Ladder cogs.
 """
 
+import os
+import re
+
 from discord.ext import commands
 from discord.message import Message
+from discord.utils import get
 
+from kiki import Kiki
 from kiki.utils import api
 
 
@@ -11,7 +16,7 @@ class Ladder(commands.Cog):
     """Ladder cog.
     """
 
-    def __init__(self, bot):
+    def __init__(self, bot: Kiki):
         self.bot = bot
 
     @commands.Cog.listener()
@@ -24,8 +29,17 @@ class Ladder(commands.Cog):
             return
 
         # Ignore messages not sent in lounge.
-        if message.channel.id != 793970769842536458:
+        if str(message.channel.id) != os.environ.get("CHANNEL_ID", "793970769842536458"):
             return
 
-        # Send the API request.
+        # Parse valid emojis from message.
+        server_id = os.environ.get("SERVER_ID", "558027628502712330")
+        emojis = re.findall(r"<:\w*:\d*>", message.content)
+        emojis = [int(e.split(':')[2].replace('>', '')) for e in emojis]
+        custom_emojis = self.bot.get_guild(int(server_id)).emojis
+        valid_emojis = [get(custom_emojis, id=e) for e in emojis]
+        emoji_ids = [str(e.id) for e in filter(lambda x: x is not None, valid_emojis)]
+
+        # Send the API requests.
         await api.post("ladder", f"/messages/{message.author.id}")
+        await api.post("ladder", f"/emotes/{message.author.id}", emoji_ids)
